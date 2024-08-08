f7dd9e4fe6f7b0628715dc06c8f9a55630dacaf1
Md. Golam Murtuja Kayes <165343080+murtuja43@users.noreply.github.com>
2024-08-08 18:08:26 +0600

    prac 1

---
+++ prac-01.py
@@ -0,0 +1,9 @@
+a = input("Enter Your Name: ")
+print("Welcome to the digital calculator, ", a)
+
+x = int(input("Type Number 1: "))
+y = int(input("Type Number 2: "))
+print("The sum of these two numbers is: ", x + y)
+print("Number 1 minus number 2 is: ", x + y)
+print("The multiply of these two numbers is: ", x + y)
+print("Number 1 divided by number 2 is: ", x + y)
