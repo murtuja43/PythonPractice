ca51c883677eb6cdaf0ebcc690e29a2dfdf79d70
Md. Golam Murtuja Kayes <165343080+murtuja43@users.noreply.github.com>
2024-08-10 15:28:35 +0600

    prac 2

---
+++ prac-02.py
@@ -0,0 +1,14 @@
+fruit1 = input("What's the 1st fruit? ")
+fruit2 = input("What's the 2nd fruit? ")
+fruit3 = input("What's the 3rd fruit? ")
+fruit4 = input("What's the 4th fruit? ")
+fruit5 = input("What's the 5th fruit? ")
+fruit6 = input("What's the 6th fruit? ")
+fruit7 = input("What's the 7th fruit? ")
+
+fruit_list = (fruit1, fruit2, fruit3, fruit4, fruit5, fruit6, fruit7)
+print("So the fruits which are well known to you locality is: ", fruit_list)
+
+
+# This was solved by me, 
+# prac 3 was solved by Harry (YouTube)
