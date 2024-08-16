b49f94a2a1cdd9e0614269eb16fe881522f1140a
Md. Golam Murtuja Kayes <165343080+murtuja43@users.noreply.github.com>
2024-08-16 23:25:40 +0600

    prac 5

---
+++ prac-05.py
@@ -0,0 +1,7 @@
+fruits = ("Orange", "Banana", "Mango", "Apple", "Grapes")
+
+fruits[1] = "Jack-Fruit"
+
+print(fruits) #this will show error
+
+# index number 1 fruit won't change, because tuple is immutable.
