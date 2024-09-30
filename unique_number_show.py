3b0fc2b70804e5e5e922379db90e13ed98aae2a1
Md. Golam Murtuja Kayes <165343080+murtuja43@users.noreply.github.com>
2024-09-30 07:55:00 +0600

    added new practice

---
+++ unique_number_show.py
@@ -0,0 +1,21 @@
+a = set()
+
+number = input("Enter you number: ")
+a.add(number)
+
+number = input("Enter you number: ")
+a.add(number)
+
+number = input("Enter you number: ")
+a.add(number)
+
+number = input("Enter you number: ")
+a.add(number)
+
+number = input("Enter you number: ")
+a.add(number)
+
+number = input("Enter you number: ")
+a.add(number)
+
+print(a)
