0dcf55ffa1fd81a80fb43f830434a561a649c901
Md. Golam Murtuja Kayes <165343080+murtuja43@users.noreply.github.com>
2024-09-30 07:48:41 +0600

    added wh_word_dictionary

---
+++ wh_word_dictionary.py
@@ -0,0 +1,13 @@
+#wh word dictionary
+
+words = {
+    "why" : "keno",
+    "how" : "kivabe",
+    "when" : "kokhon",
+    "what" : "ki",
+    "kemon" : "how"
+}
+
+search = input("Search the wh words meaning: ")
+
+print(words[search])
