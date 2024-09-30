f768e2c91368b3496ec4b5e5db393dbce7a4a65d
Md. Golam Murtuja Kayes <165343080+murtuja43@users.noreply.github.com>
2024-09-30 08:13:32 +0600

    take friends favourite language input file

---
+++ favourite_laguage_input.py
@@ -0,0 +1,21 @@
+a = {}
+
+name = input("Enter friends name: ")
+lang = input(f"Enter {name}'s language name: ")
+a.update({name : lang})
+
+
+name = input("Enter friends name: ")
+lang = input(f"Enter {name}'s language name: ")
+a.update({name : lang})
+
+name = input("Enter friends name: ")
+lang = input(f"Enter {name}'s language name: ")
+a.update({name : lang})
+
+name = input("Enter friends name: ")
+lang = input(f"Enter {name}'s language name: ")
+a.update({name : lang})
+
+
+print(a)
